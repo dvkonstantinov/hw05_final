06cf49eef7a5332aa22a15f1e114ae91477100ca
Dmitry Konstantinov <ensifko@yandex.ru>
2022-02-16 08:33:47 +0300

    v2

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -225,7 +225,7 @@ class PostPagesTest(TestCase):
     def test_unable_follow_yourself(self):
         count_follow = Follow.objects.count()
         print(count_follow)
-        response = self.authorized_client.get(
+        self.authorized_client.get(
             reverse('posts:profile_follow',
                     kwargs={'username': self.user.username}))
         count_follow = Follow.objects.count()
